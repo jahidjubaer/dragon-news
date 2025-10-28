406918f33659abaf76ccf31db29798d0b2efbaf4
Jahid Hasan <jahidjubaer17@gmail.com>
2025-10-28 10:30:11 +0600

    env setup for firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDdFSSE8H7eldo21vYakArqHT_geZYLkdk",
-  authDomain: "dragon-news-1f692.firebaseapp.com",
-  projectId: "dragon-news-1f692",
-  storageBucket: "dragon-news-1f692.firebasestorage.app",
-  messagingSenderId: "734771930400",
-  appId: "1:734771930400:web:9771264fb5bc97713e6345",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
